2ac114ae1e154ab1826824643e83c9bb7a687d4c
KVG24 <141866132+KVG24@users.noreply.github.com>
2023-10-16 11:49:24 +0300

    Fix code and browser messages clarity

--- script.js
+++ script.js
@@ -3,58 +3,57 @@ function getComputerChoice() {
     return arr[Math.floor(Math.random() * arr.length)];
 };
 
-function playerInteraction() {
+function getPlayerChoice() {
     let choice = prompt('Choose rock, paper or scissors').toLowerCase();
     if (choice === '' || choice === null) {
         return alert(`You didn't choose anything`)
-    } else if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+    } else if (choice !== 'rock' && 
+                choice !== 'paper' && 
+                choice !== 'scissors') {
         return alert('Probably a typo')
     } else {
         return choice
     }
 };
 
-function playRound(a, b) {    
-    if (a === 'paper' && b === 'rock') {
+function playRound(human, cpu) {    
+    if (human === 'paper' && cpu === 'rock') {
         return 'won'
-    } else if (a === 'paper' && b === 'scissors') {
+    } else if (human === 'paper' && cpu === 'scissors') {
         return 'lost'
-    } else if (a === 'scissors' && b === 'rock') {
+    } else if (human === 'scissors' && cpu === 'rock') {
         return 'lost'
-    } else if (a === 'scissors' && b === 'paper') {
+    } else if (human === 'scissors' && cpu === 'paper') {
         return 'won'
-    } else if (a === 'rock' && b === 'paper') {
+    } else if (human === 'rock' && cpu === 'paper') {
         return 'lost'
-    } else if (a === 'rock' && b === 'scissors') {
+    } else if (human === 'rock' && cpu === 'scissors') {
         return 'won'
-    } else if (a === b) {
+    } else if (human === cpu) {
         return `tie`
     } else { return `Something went wrong`}
 };
-
 
 function game() {
     let userScore = 0;
     let cpuScore = 0;
     
     for (let i = 0; i < 5; i++) {
-        let playerChoice = playerInteraction();
-        let computerChoice = getComputerChoice().toLowerCase();
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice().toLowerCase();       
+        let result = playRound(playerChoice, computerChoice);
         
-        console.log(playerChoice);
-        console.log(computerChoice);        
-        
-        let result = playRound(playerChoice, computerChoice)
-        
-        if (result === 'won') {
-            userScore++
-        } else if (result === 'lost') {
-            cpuScore++
-        } else {
-            i--
-        }
-        alert(`User  ${userScore} : ${cpuScore}  CPU`)
+            if (result === 'won') {
+                userScore++
+            } else if (result === 'lost') {
+                cpuScore++
+            } else {
+                i--
+            }
+        alert('User  -  ' + playerChoice + '   ' + userScore + '   :   ' 
+            + cpuScore + '   ' + computerChoice + '  -  CPU')
     }
+    
     if (userScore > cpuScore) {
         return alert('You won!')
     } else if (userScore < cpuScore) {
